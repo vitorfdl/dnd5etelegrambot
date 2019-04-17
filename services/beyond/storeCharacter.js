68da713d12971bf27dba29c9a4b84b50eaec013c
Vitor Lima <vitor@ferreiradelima.com>
2019-04-17 00:06:22 -0300

    /personagem change

--- services/beyond/storeCharacter.js
+++ services/beyond/storeCharacter.js
@@ -2,8 +2,10 @@ const storage = require('./lib/storage');
 const getCharacter = require('./getCharacter');
 
 module.exports = async (bot, msg, link) => {
-  if (!link.includes('https://www.dndbeyond.com/')) {
-    return bot.sendMessage(msg.chat.id, 'Link inválido. Utilize uma rota do Beyond DnD 5e.');
+  if (!link) {
+    return bot.sendStructedMessage(msg.chat.id, 'Sintaxe incorreta. Use `/personagem <link>`.');
+  } else if (!link.includes('https://www.dndbeyond.com/')) {
+    return bot.sendStructedMessage(msg.chat.id, 'Link inválido. Utilize uma rota do Beyond DnD 5e.\nUse `/personagem <link>`.');
   }
 
   const data = await getCharacter(bot, msg, msg.from.id, link);
@@ -12,5 +14,5 @@ module.exports = async (bot, msg, link) => {
   storage.save(msg.from.id, link);
 
   const quote = `<a href="tg://user?id=${msg.from.id}">${msg.from.first_name}</a>`;
-  bot.sendMessage(msg.chat.id, `O personagem ${data.name} foi associado a você ${quote}`, { parse_mode: 'HTML', disable_notification: true });
+  bot.sendStructedMessage(msg.chat.id, `O personagem ${data.name} foi associado a você ${quote}`);
 };
