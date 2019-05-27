0b3ca62ba681ecfc13cc05ec9c27b09ef39c4bc4
Vitor Lima <vitor@ferreiradelima.com>
2019-05-27 15:12:34 -0300

    fixed /init round

--- services/initative/round.js
+++ services/initative/round.js
@@ -7,6 +7,14 @@ module.exports = async (bot, msg) => {
 
   my_list.round = (my_list.round || 0) + 1;
   my_list.turn = 0;
+  for (let i = 0; i <= my_list.creatures.length; i += 1) {
+    if (my_list.turn > my_list.creatures.length - 1) my_list.turn = 0;
+
+    const m = my_list.creatures[my_list.turn];
+    if ((m.hp > 0 || !m.max_hp) && !m.death && !m.constrained) break;
+
+    my_list.turn += 1;
+  }
 
   await initLoader.save(msg.chat.id, my_list.name, my_list);
   return initList(bot, msg, [], my_list);
